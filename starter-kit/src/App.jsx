4acf79eec8677af6857e1ae8471f11323a6c86fa
Ayush Kumar Verma <ayushkumarverma97@outlook.com>
2024-02-14 01:43:39 +0530

    Added starter kit

---
+++ starter-kit/src/App.jsx
@@ -0,0 +1,23 @@
+import React, { lazy, Suspense } from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+
+// home pages  & dashboard
+//import Dashboard from "./pages/dashboard";
+const Dashboard = lazy(() => import("./pages/dashboard"));
+
+import Layout from "./layout/Layout";
+import AuthLayout from "./layout/AuthLayout";
+
+function App() {
+  return (
+    <main className="App  relative">
+      <Routes>
+        <Route path="/*" element={<Layout />}>
+          <Route path="dashboard" element={<Dashboard />} />
+        </Route>
+      </Routes>
+    </main>
+  );
+}
+
+export default App;
